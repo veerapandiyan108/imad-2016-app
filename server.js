16c57530e3232639d10318c2f50472a9ea264a04
veerapandiyan108 <daddyinfotech@gmail.com>
2016-10-12 09:52:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('article two registred and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three', function (req, res) {
-  res.send('article three registred and will be served here');
+app.get('/article-third', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-third.html'));
 });
 
 
