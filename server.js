c123c32b0a659ae41eaa5357aa47b3504b1c244d
veerapandiyan108 <daddyinfotech@gmail.com>
2016-10-12 08:47:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,25 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
-
-app.get('/artical-one ',funciton (req,res){
- res.send('artical one requested and will be served here ');
-});
-
-app.get('/artical-one ',funciton (req,res){
- res.send('artical one requested and will be served here ');
-});
-
-app.get('/artical-one ',funciton (req,res){
- res.send('artical one requested and will be served here ');
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
