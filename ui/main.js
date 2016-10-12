6dad4242de66fac6898555af8d6b14b193d082e6
veerapandiyan108 <daddyinfotech@gmail.com>
2016-10-12 23:08:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element =document.getElementById('main-text');
 element.innerHtml='New Value';
 
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.OnClick=function(){
     img.style.marginleft='100px';
 };
