05f977b4a257bb6d54d49fa4bc45b86ecc16a2f3
veerapandiyan108 <daddyinfotech@gmail.com>
2016-10-12 09:06:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/ui/style.css',function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/madi.png',function (req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
